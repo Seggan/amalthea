94694cacf5b478f97f82e0af19345f67dde374e7
Seggan <seggan21@gmail.com>
2025-08-29 13:40:46 -0500

    Add instrinsics processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,21 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+val compileIntrinsics by tasks.registering(JavaCompile::class) {
+    source = sourceSets.main.get().resources
+    include("Intrinsics.java")
+    classpath = files()
+    sourceCompatibility = "21"
+    destinationDirectory = layout.buildDirectory.dir("generated/intrinsics")
+}
+
+tasks.processResources {
+    dependsOn(compileIntrinsics)
+    from(compileIntrinsics) {
+        include("Intrinsics.class")
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
---
+++ src/main/resources/Intrinsics.java
@@ -0,0 +1,7 @@
+package io.github.seggan.amalthea.intrinsics;
+
+class Intrinsics {
+    public static void println(String message) {
+        System.out.println(message);
+    }
+}
